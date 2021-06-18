c7be072f57468b2df736cb562ba2da8acdc39511
Al Amin <ialamin.pro@gmail.com>
2021-06-18 19:23:14 +0600

    Update payment method

--- stripe/PaymentMethod.php
+++ stripe/PaymentMethod.php
@@ -5,10 +5,19 @@ require_once('StripeInterface.php');
 
 class PaymentMethod extends BaseApi implements StripeInterface {
 
+    /**
+     * declared stripe payment methods api end point
+     */
     const END_POINT = '/payment_methods';
 
+    /**
+     * @var Curl
+     */
     private $curl;
 
+    /**
+     * Customer constructor.
+     */
     public function __construct()
     {
         $this->curl = new Curl();
@@ -16,9 +25,11 @@ class PaymentMethod extends BaseApi implements StripeInterface {
 
     /**
      * @param $data
-     * @return bool|string
+     * @return mixed
      */
     public function create($data){
+
+        if(!is_array($data)) return self::INVALID_DATA;
 
         return $this->curl->setBaseUrl(self::END_PONT)
             ->setMethod(self::POST)
@@ -28,9 +39,11 @@ class PaymentMethod extends BaseApi implements StripeInterface {
 
     /**
      * @param $id
-     * @return bool|string
+     * @return mixed
      */
     public function retrieve($id){
+
+        if(!is_string($id)) return self::INVALID_DATA;
 
         return $this->curl->setBaseUrl(self::END_PONT.'/'.$id)
             ->setMethod(self::GET)
@@ -39,22 +52,32 @@ class PaymentMethod extends BaseApi implements StripeInterface {
 
     /**
      * @param $id
-     * @param $data
-     * @return bool|string
+     * @param $orderId
+     * @return string
      */
-    public function update($id, $data){
+    public function update($id, $orderId){
+
+        if(!is_string($id) || !is_string($orderId)) return self::INVALID_DATA;
 
         return $this->curl->setBaseUrl(self::END_PONT.'/'.$id)
             ->setMethod(self::PUT)
-            ->setData($data)
+            ->setData(
+                [
+                    'metadata' => [
+                        'order_id' => $orderId
+                    ]
+                ]
+            )
             ->sendDataToStripeApi();
     }
 
     /**
      * @param $id
-     * @return bool|string
+     * @return mixed
      */
     public function delete($id){
+
+        if(!is_string($id)) return self::INVALID_DATA;
 
         return $this->curl->setBaseUrl(self::END_PONT.'/'.$id)
             ->setMethod(self::DELETE)
@@ -62,9 +85,12 @@ class PaymentMethod extends BaseApi implements StripeInterface {
     }
 
     /**
-     * @return bool|string
+     * @param int $limit
+     * @return mixed
      */
-    public function all(){
+    public function all($limit = 3){
+
+        if(!is_int($limit)) return self::INVALID_DATA;
 
         return $this->curl->setBaseUrl(self::END_PONT)
             ->setMethod(self::GET)
@@ -73,26 +99,33 @@ class PaymentMethod extends BaseApi implements StripeInterface {
 
     /**
      * @param $id
-     * @param $data
-     * @return bool|string
+     * @param $customerId
+     * @return mixed
      */
-    public function attach($id, $data){
+    public function attach($id, $customerId){
+
+        if(!is_string($id) || !is_string($customerId)) return self::INVALID_DATA;
 
         return $this->curl->setBaseUrl(self::END_PONT.'/'.$id.'/attach')
             ->setMethod(self::POST)
-            ->setData($data)
+            ->setData(
+                [
+                    'customer' => $customerId
+                ]
+            )
             ->sendDataToStripeApi();
     }
 
     /**
      * @param $id
-     * @return bool|string
+     * @return mixed
      */
     public function detach($id){
 
+        if(!is_string($id)) return self::INVALID_DATA;
+
         return $this->curl->setBaseUrl(self::END_PONT.'/'.$id.'/detach')
             ->setMethod(self::POST)
-            ->setData([])
             ->sendDataToStripeApi();
     }
 }
